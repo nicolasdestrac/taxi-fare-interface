e08c7dde8709abadba1fd32cf77de292b2eae724
NicolasD <nicolas.destrac@gmail.com>
2024-03-01 14:20:41 +0100

    v1

--- script.js
+++ script.js
@@ -206,8 +206,8 @@ const predict = () => {
       .then(response => response.json())
       .then(data => {
         document.getElementById('fare_amount').classList.remove('d-none');
-        const fareResult = document.getElementById('predicted-fare');
-        const fare = Math.round(data['fare'] * 100) / 100
+        const fareResult = document.getElementById('fare_amount');
+        const fare = Math.round(data['fare_amount'] * 100) / 100
         fareResult.innerText = `$${fare}`;
       })
       .catch((error) => {
